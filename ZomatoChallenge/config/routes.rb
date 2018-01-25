f93f21531d8efc3dca9f446eeadde4ff157e8e5d
Wilfred Ruck <wilfredoruck@gmail.com>
2018-01-25 16:17:31 -0500

    fix routes

--- ZomatoChallenge/config/routes.rb
+++ ZomatoChallenge/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'food#index'
-  get 'food/index'
+  get '/', to: 'food#index'
 
-  get 'food/show'
+  get '/show', to: 'food#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
